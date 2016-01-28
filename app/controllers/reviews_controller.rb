bdf23bbc7dc685269f4a106b291e342b0c669c80
tanner466 <tanner466@gmail.com>
2016-01-28 16:31:21 -0500

    Adding user names

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :ensure_logged_in, only: [:create, :destroy]
+
   before_action :load_product
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+
+  validates :name, presence: true
+
   has_many :reviews
   has_many :products, through: :reviews
 end
