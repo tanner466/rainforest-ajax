daf087a2905afaaefd1c1527f2d472336464c151
tanner466 <tanner466@gmail.com>
2016-01-28 14:41:53 -0500

    Creating a user model, controller and view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
+
+  def formatted_price
+    price_in_dollars = price_in_cents / 100.0
+    sprintf("%.2f", price_in_dollars)
+  end
 end
