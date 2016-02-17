362339aa265478f982dc40d74900cfc4dc79cf1d
tanner466 <tanner466@gmail.com>
2016-02-17 15:06:39 -0500

    Use javascript form.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -40,4 +40,14 @@ $(document).on('ready page:load', function() {
     //   });
     $.getScript('/products?search=' + searchValue);
   });
+
+  $(window).scroll(function() {
+    var url = $('.pagination span.next').children().attr('href');
+
+    if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+      $('.pagination').text("Fetching more products...");
+      return $.getScript(url);
+
+    }
+  });
 });
